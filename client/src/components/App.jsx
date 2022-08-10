95d9cffd180af08400e8ec8b5d25190d2e7ec23e
Michael Schoenecker <michael.schoenecker@gmail.com>
2022-08-09 20:13:49 -0400

    initial setup of frontend components

---
+++ client/src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+//Components
+import Header from './Header.jsx';
+import Body from './Body.jsx';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+    </>
+  );
+};
+
+export default App;
---
+++ client/src/components/Body.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import axios from 'axios';
+
+const Body = () => {
+  return (
+    <body>
+      <h1>Here's A Kana!</h1>
+    </body>
+  );
+};
+
+export default Body;
